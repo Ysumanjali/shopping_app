770384b6dfcd795fbb539110f3c05d68471a826c
Yarabolu Sumanth Babu <yarabolusumanjali@gmail.com>
2024-06-17 14:20:58 +0530

    Initial commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Contact = () => {
+  return <div>
+     <div id="footer">
+    <div>
+      <h4 class="footer_heading">Company</h4>
+      <div class="footer_content">
+        <div>About Us</div>
+        <div>Careers</div>
+        <div>Compliance</div>
+        <div>Privacy Policy</div>
+        <div>Contact Us</div>
+        <div>Disclaimer</div>
+      </div>
+    </div>
+  </div>
+  </div>;
+};
